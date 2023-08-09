79372434519163f3f529bc4d0244a5570ace1146
frederic smée <ssdfred@yahoo.fr>
2023-08-09 13:28:56 +0200

    htacess

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\HoraireRepository;
 use App\Repository\ServiceRepository;
 use App\Repository\VoitureRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
